53400a404d87dac81cbc77a2bed8f5b4d3bb17e0
thairelife <thairelifewebsite@gmail.com>
2022-06-10 20:41:15 +0700

    Init Proj

---
+++ FileConverter/ConvertPDF.aspx.cs
@@ -0,0 +1,267 @@
+﻿using aspose = Aspose.Pdf;
+using Ionic.Zip;
+//using Spire.Pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing.Imaging;
+
+namespace FileConverter
+{
+    public partial class _ConvertPDF : Page
+    {
+        //public Dictionary<string, byte[]> Files
+        //{
+        //    get
+        //    {
+        //        if (Session[hidSessionID.Value] == null)
+        //            Session[hidSessionID.Value] = new Dictionary<string, byte[]>();
+
+        //        return (Dictionary<string, byte[]>)Session[hidSessionID.Value];
+        //    }
+        //    set
+        //    {
+        //        Session[hidSessionID.Value] = value;
+        //    }
+        //}
+        public Dictionary<string, byte[]> Files
+        {
+            get
+            {
+                if (Session["Files"] == null)
+                    Session["Files"] = new Dictionary<string, byte[]>();
+
+                return (Dictionary<string, byte[]>)Session["Files"];
+            }
+            set
+            {
+                if (value == null) Session.Remove("Files");
+                Session["Files"] = value;
+            }
+        }
+        //public string FileType
+        //{
+        //    get
+        //    {
+        //        if (Response.Cookies["FileType"] == null)
+        //            Session["FileType"] = "Excel";
+
+        //        return Session["FileType"].ToString();
+        //    }
+        //    set
+        //    {
+        //        if (value == null) Session.Remove("FileType");
+        //        Session["FileType"] = value;
+        //    }
+        //}
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack && !Page.IsCallback)
+            {
+                Files = null;
+                Response.Cookies["FileType"].Value = ddlFileType.SelectedValue;
+            }
+        }
+
+        protected void btnConvert_Click(object sender, EventArgs e)
+        {
+
+        }
+        void AddToExcel(string fileName, byte[] bytes)
+        {
+            //var file = fuAttachment.PostedFile;
+            //if (file != null)
+            //{
+            //    var content = new byte[file.ContentLength];
+            //    file.InputStream.Read(content, 0, content.Length);
+            //    Session["FileContent"] = content;
+            //    Session["FileContentType"] = file.ContentType;
+            //}
+            lock (Files)
+            {
+                //var files = Files;
+                //PdfDocument pdfDocument = new PdfDocument(bytes);
+                //string outFileName = Path.GetFileNameWithoutExtension(fileName) + ".xlsx";
+                //pdfDocument.ConvertOptions.SetPdfToXlsxOptions(PdfToXlsxLayout.Text);
+                //using (MemoryStream pdfStream = new MemoryStream())
+                //{
+                //    pdfDocument.SaveToStream(pdfStream, FileFormat.XLSX);
+                //    if (files.ContainsKey(outFileName))
+                //    {
+                //        files[outFileName] = pdfStream.ToArray();
+                //    }
+                //    else
+                //    {
+                //        files.Add(outFileName, pdfStream.ToArray());
+                //    }
+                //    // Dispose memory
+                //    pdfStream.Dispose();
+                //}
+                //// Dispose memory
+                //pdfDocument.Dispose();
+                //Files = files;
+
+                aspose.ExcelSaveOptions saveOptions = new aspose.ExcelSaveOptions();
+                //saveOptions.UniformWorksheets = true;
+                //saveOptions.ConversionEngine = aspose.ExcelSaveOptions.ConversionEngines.LegacyEngine;
+                //saveOptions.ScaleFactor = 2.5;
+                saveOptions.Format = aspose.ExcelSaveOptions.ExcelFormat.XLSX;
+
+                var files = Files;
+                string outFileName = Path.GetFileNameWithoutExtension(fileName) + ".xlsx";
+                MemoryStream stream = new MemoryStream(bytes);
+                aspose.Document pdfDocument = new aspose.Document(stream);
+
+                List<string> list = new List<string>();
+                foreach (var page in pdfDocument.Pages)
+                {
+                    foreach (var img in page.Resources.Images)
+                    {
+                        var str = CallBackGetHocr(img);
+                        list.Add(str);
+                    }
+                }
+                using (MemoryStream pdfStream = new MemoryStream())
+                {
+                    pdfDocument.Save(pdfStream, saveOptions);//aspose.SaveFormat.DocX,
+                    files.Add(outFileName, pdfStream.ToArray());
+                    // Dispose memory
+                    pdfStream.Dispose();
+                }
+                // Dispose memory
+                pdfDocument.Dispose();
+                Files = files;
+                // Save the file into MS document format
+                stream.Dispose();
+            }
+        }
+        static string CallBackGetHocr(aspose.XImage img)
+        {
+            FileStream outputImage = new FileStream(@"D:\Test\"+ DateTime.Now.ToString("HHmmssfff") + ".jpg", FileMode.Create);
+
+            //save output image
+            img.Save(outputImage, ImageFormat.Png);
+            outputImage.Close();
+            return "";
+            // Initialize an instance of OcrEngine
+            //Aspose.OCR.OcrEngine ocrEngine = new Aspose.OCR.OcrEngine();
+            //var ms = new MemoryStream();
+
+            //img.Save(ms, ImageFormat.Jpeg); // put here the image format 
+            //ms.Position = 0;
+
+
+            //// Set the Image property by loading the image from file path location or an instance of MemoryStream 
+            //ocrEngine.Image = Aspose.OCR.ImageStream.FromStream(ms, Aspose.OCR.ImageStreamFormat.Jpg);
+
+            //    // Process the image
+            //    ocrEngine.Process();
+            //string text = ocrEngine.Text.ToString();
+            //return text;
+        }
+        void AddToWord(string fileName, byte[] bytes, aspose.DocSaveOptions.RecognitionMode recognitionMode)
+        {
+            lock (Files)
+            {
+                //var files = Files;
+                //PdfDocument pdfDocument = new PdfDocument(bytes);
+                //string outFileName = Path.GetFileNameWithoutExtension(fileName) + ".docx";
+                //pdfDocument.ConvertOptions.SetPdfToDocOptions(true, useFlowRecognitionMode);
+                //using (MemoryStream pdfStream = new MemoryStream())
+                //{
+                //    pdfDocument.SaveToStream(pdfStream, FileFormat.DOCX);
+                //    files.Add(outFileName, pdfStream.ToArray());
+                //    // Dispose memory
+                //    pdfStream.Dispose();
+                //}
+                //// Dispose memory
+                //pdfDocument.Dispose();
+                //Files = files;
+
+
+                aspose.DocSaveOptions saveOptions = new aspose.DocSaveOptions();
+                saveOptions.Mode = recognitionMode;
+                //saveOptions.RelativeHorizontalProximity = 2.5f;
+                saveOptions.RecognizeBullets = true;
+                saveOptions.Format = aspose.DocSaveOptions.DocFormat.DocX;
+
+                var files = Files;
+                string outFileName = Path.GetFileNameWithoutExtension(fileName) + ".docx";
+                MemoryStream stream = new MemoryStream(bytes);
+                aspose.Document pdfDocument = new aspose.Document(stream);
+                using (MemoryStream pdfStream = new MemoryStream())
+                {
+                    pdfDocument.Save(pdfStream, saveOptions);//aspose.SaveFormat.DocX,
+                    files.Add(outFileName, pdfStream.ToArray());
+                    // Dispose memory
+                    pdfStream.Dispose();
+                }
+                // Dispose memory
+                pdfDocument.Dispose();
+                Files = files;
+                // Save the file into MS document format
+                stream.Dispose();
+            }
+        }
+
+
+        protected void ajaxUploads_UploadComplete(object sender, AjaxControlToolkit.AjaxFileUploadEventArgs e)
+        {
+            switch (Request.Cookies["FileType"].Value)
+            {
+                case "Excel":
+                    AddToExcel(e.FileName, e.GetContents());
+                    break;
+                case "WordPDFLayout":
+                    AddToWord(e.FileName, e.GetContents(), aspose.DocSaveOptions.RecognitionMode.Flow);
+                    break;
+                case "WordTextLayout":
+                    AddToWord(e.FileName, e.GetContents(), aspose.DocSaveOptions.RecognitionMode.EnhancedFlow);
+                    break;
+            }
+            e.DeleteTemporaryData();
+        }
+
+        protected void btnDownload_Click(object sender, EventArgs e)
+        {
+            if (Files.Count == 1)
+            {
+                var file = Files.First();
+                Response.AppendHeader("content-disposition", "attachment; filename=" + file.Key);
+                Response.ContentType = "application/octetstream";
+                Response.Clear();
+                Response.OutputStream.Write(file.Value, 0, file.Value.Length);
+            }
+            else
+            {
+                ZipFile zip = new ZipFile();
+                foreach (var stream in Files)
+                {
+                    zip.AddEntry(stream.Key, stream.Value);
+                }
+
+                zip.Dispose();
+
+                Response.AppendHeader("content-disposition", "attachment; filename=" + "result.zip");
+                Response.ContentType = "application/octetstream";
+                Response.Clear();
+                zip.Save(Response.OutputStream);
+            }
+
+            Response.Flush();
+            Files = null;
+            Response.End();
+        }
+
+        protected void ddlFileType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Response.Cookies["FileType"].Value = ddlFileType.SelectedValue;
+            //FileType = ddlFileType.SelectedValue;
+        }
+    }
+}
---
+++ FileConverter/obj/Release/Package/PackageTmp/ConvertPDF.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ConvertPDF.aspx.cs" Inherits="FileConverter._ConvertPDF" %>
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="asp" %>
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <script type="text/javascript">
+
+     $(document).ready(function () {
+         AjaxFileUpload_change_text();
+
+     });
+
+     function AjaxFileUpload_change_text() {
+        //document.getElementsByClassName('ajax__fileupload_selectFileButton')[0].innerHTML = "Abrir";
+        //document.getElementsByClassName('ajax__fileupload_dropzone')[0].innerHTML = "Arrastre archivo aquí";
+        document.getElementsByClassName('ajax__fileupload_uploadbutton')[0].innerHTML = "Convert";
+        //document.getElementById("ctl00_MainContent_AjaxFileUpload1_FileStatusContainer").innerHTML = "Seleccione archivo(s) a subir.";
+
+         //Sys.Extended.UI.Resources.AjaxFileUpload_SelectFile = "Select File";
+         //Sys.Extended.UI.Resources.AjaxFileUpload_DropFiles = "Drop files here";
+         //Sys.Extended.UI.Resources.AjaxFileUpload_Pending = "pending";
+         //Sys.Extended.UI.Resources.AjaxFileUpload_Remove = "Remove";
+         Sys.Extended.UI.Resources.AjaxFileUpload_Upload = "Convert";
+         //Sys.Extended.UI.Resources.AjaxFileUpload_Uploaded = "Uploaded";
+         //Sys.Extended.UI.Resources.AjaxFileUpload_UploadedPercentage = "uploaded {0} %";
+         //Sys.Extended.UI.Resources.AjaxFileUpload_Uploading = "Uploading";
+         //Sys.Extended.UI.Resources.AjaxFileUpload_FileInQueue = "{0} file(s) in queue.";
+         //Sys.Extended.UI.Resources.AjaxFileUpload_AllFilesUploaded = "All Files Uploaded.";
+         //Sys.Extended.UI.Resources.AjaxFileUpload_FileList = "List of Uploaded files:";
+         //Sys.Extended.UI.Resources.AjaxFileUpload_SelectFileToUpload = "Please select file(s) to upload.";
+         //Sys.Extended.UI.Resources.AjaxFileUpload_Cancelling = "Cancelling...";
+         //Sys.Extended.UI.Resources.AjaxFileUpload_UploadError = "An Error occured during file upload.";
+         //Sys.Extended.UI.Resources.AjaxFileUpload_CancellingUpload = "Cancelling upload...";
+         //Sys.Extended.UI.Resources.AjaxFileUpload_UploadingInputFile = "Uploading file: {0}.";
+         //Sys.Extended.UI.Resources.AjaxFileUpload_Cancel = "Cancel";
+         //Sys.Extended.UI.Resources.AjaxFileUpload_Canceled = "cancelled";
+         //Sys.Extended.UI.Resources.AjaxFileUpload_UploadCanceled = "File upload cancelled";
+         //Sys.Extended.UI.Resources.AjaxFileUpload_DefaultError = "File upload error";
+         //Sys.Extended.UI.Resources.AjaxFileUpload_UploadingHtml5File = "Uploading file: {0} of size {1} bytes.";
+         //Sys.Extended.UI.Resources.AjaxFileUpload_error = "error";
+     }
+
+     function uploadCompleteAll(sender, args) {
+                document.getElementById("<%=btnDownload.ClientID %>").click();
+       }
+</script>
+    <div class="jumbotron">
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server"><ContentTemplate>
+             Convert PDF to <asp:DropDownList ID="ddlFileType" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlFileType_SelectedIndexChanged">
+            <asp:ListItem>Excel</asp:ListItem>
+            <asp:ListItem Value="WordPDFLayout">Word (PDF Layout)</asp:ListItem>
+            <asp:ListItem Value="WordTextLayout">Word (Text Layout)</asp:ListItem>
+        </asp:DropDownList><br /><br />
+                                                          </ContentTemplate></asp:UpdatePanel>
+       
+    <asp:AjaxFileUpload ID="ajaxUploads" runat="server"
+         OnUploadComplete="ajaxUploads_UploadComplete" AllowedFileTypes="pdf" OnClientUploadCompleteAll="uploadCompleteAll" ContextKeys=""  />
+        <asp:Button ID="btnDownload" runat="server" Text="Download" OnClick="btnDownload_Click" style="display:none" />
+        <div>
+    </div>
+
+</div>
+</asp:Content>
